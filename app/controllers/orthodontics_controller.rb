864ccbc4347ba170eab26457064a76670ad8cf77
Json Manz <jsonmanz@gmail.com>
2013-01-31 21:54:17 -0800

    modified link

--- app/controllers/orthodontics_controller.rb
+++ app/controllers/orthodontics_controller.rb
@@ -1,61 +1,56 @@
 class OrthodonticsController < ApplicationController
 
-layout 'admin'
+	layout 'admin'
 
-before_filter :confirm_logged_in
-before_filter :confirm_admin, :except => [:list, :new, :create, :show]
-before_filter :find_schedules
-before_filter :find_pendings
+	before_filter :confirm_logged_in
+	before_filter :confirm_admin, :except => [:index, :new, :create, :show]
+	before_filter :find_schedules
+	before_filter :find_pendings
 
-def index
-	@orthodontics = Orthodontic.search(params[:search], params[:page])
-end
-
-def list
-	@orthodontics = Orthodontic.all
-	# @orthodontics = Orthodontic.search(params[:search], params[:page])
-end
-
-def show
-	@orthodontic = Orthodontic.find(params[:id])
-end
-
-def new
-	@orthodontic = Orthodontic.new
-end
-
-def create
-	@orthodontic = Orthodontic.new(params[:ortho])
-	if @orthodontic.save
-		redirect_to(:action => 'list')	
-	else
-		@full_name = params[:full_name]
-		render :template => 'orthodontics/new'
+	def index
+		@orthodontics = Orthodontic.search(params[:search], params[:page])
 	end
-end
 
-def edit
-	@orthodontic = Orthodontic.find(params[:id])
-end
-
-def update
-	@orthodontic = Orthodontic.find(params[:id])
-	if @orthodontic.update_attributes(params[:orthodontic])
-		redirect_to(:action => 'show', :id => @orthodontic.id)	
-	else
-		render('edit')
+	def show
+		@orthodontic = Orthodontic.find(params[:id])
 	end
-end
 
-def delete
-	@orthodontic = Orthodontic.find(params[:id])
-end
+	def new
+		@orthodontic = Orthodontic.new
+	end
 
-def destroy
-	orthodontic = Orthodontic.find(params[:id])
-	orthodontic.delete
-	flash[:notice] = "Orthopedic patient destroyed."
-	redirect_to(:action => 'list')
-end
+	def create
+		@orthodontic = Orthodontic.new(params[:ortho])
+		if @orthodontic.save
+			redirect_to(:action => 'list')	
+		else
+			@full_name = params[:full_name]
+			render :template => 'orthodontics/new'
+		end
+	end
+
+	def edit
+		@orthodontic = Orthodontic.find(params[:id])
+	end
+
+	def update
+		@orthodontic = Orthodontic.find(params[:id])
+		if @orthodontic.update_attributes(params[:orthodontic])
+			redirect_to(:action => 'show', :id => @orthodontic.id)	
+		else
+			render('edit')
+		end
+	end
+
+	def delete
+		@orthodontic = Orthodontic.find(params[:id])
+	end
+
+	def destroy
+		orthodontic = Orthodontic.find(params[:id])
+		orthodontic.delete
+		flash[:notice] = "Orthopedic patient destroyed."
+		redirect_to(:action => 'list')
+	end
 
 end
